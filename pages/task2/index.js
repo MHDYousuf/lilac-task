d6ad5e1221a6007023d57a537175de8782b14675
MHDYousuf <mohd4yousuf@gmail.com>
2021-03-12 15:12:46 +0530

    task 2 completed

--- pages/task2/index.js
+++ pages/task2/index.js
@@ -1,10 +1,34 @@
+import { Avatar, Container, Grid, Typography } from "@material-ui/core";
 import React from "react";
 
 function Task2() {
 	return (
-		<div>
-			<p>Task 2</p>
-		</div>
+		<Container style={{ padding: "2rem", background: "#f0f3f7" }}>
+			{[1, 2, 3, 4, 5, 6].map((item) => (
+				<Grid
+					key={item}
+					container
+					style={
+						item > 2
+							? { padding: 20, background: "#fff", alignItems: "center" }
+							: { padding: 20, background: "#c7d7f0", alignItems: "center" }
+					}
+				>
+					<Avatar
+						alt="Remy Sharp"
+						src="https://picsum.photos/200"
+						style={{ margin: "0px 15px", width: 50, height: 50 }}
+					/>
+					<span>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+						nesciunt!
+						<Typography variant="subtitle2" style={{ margin: "5px 0px" }}>
+							1 hr ago
+						</Typography>
+					</span>
+				</Grid>
+			))}
+		</Container>
 	);
 }
 
